a840d465802511a43f90c93006b9c8eb4cddcb86
Aditi Gupta <aditi251gupta@gmail.com>
2025-04-01 23:28:31 +0530

    Time: 3 ms (90.18%) | Memory: 44.5 MB (56.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if (s.length() != t.length()) return false;
-
-        int[] count = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
-
-        for (int c : count) {
-            if (c != 0) return false;
-        }
-
-        return true;
-    }
-}
-
-        /*if (s.length() != t.length()) {
+        if (s.length() != t.length()) {
             return false;
         }
         
@@ -30,4 +13,4 @@ class Solution {
         
         return Arrays.equals(sArray, tArray);
     }
-}*/
+}
