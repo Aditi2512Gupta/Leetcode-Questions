489d13740c63e8f05bbd620cfc6f238f6251691a
Aditi Gupta <aditi251gupta@gmail.com>
2025-04-02 22:35:49 +0530

    Time: 3 ms (74.23%) | Memory: 42.6 MB (99.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,31 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        StringBuilder filtered = new StringBuilder();
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            // Move left pointer forward if it's not alphanumeric
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            // Move right pointer backward if it's not alphanumeric
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare characters (convert to lowercase)
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+
+
+       /* StringBuilder filtered = new StringBuilder();
         
         for (char c : s.toCharArray()) {
             if (Character.isLetterOrDigit(c)) {
@@ -14,8 +38,8 @@ class Solution {
             if (Character.isLetterOrDigit(c)) {
                 filtered += Character.toLowerCase(c); // Creates a new String object each time
             }
-        }*/
+        }
 
-        return filtered.toString().equals(filtered.reverse().toString());
+        return filtered.toString().equals(filtered.reverse().toString());*/
     }
 }
