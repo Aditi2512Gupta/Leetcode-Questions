c1a34bc37ca05c6ca0718d7b90ea18f9a3adda65
Aditi Gupta <aditi251gupta@gmail.com>
2025-04-07 22:31:32 +0530

    Time: 29 ms (71.08%) | Memory: 42.6 MB (85.98%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,25 +1,16 @@
 class Solution {
     public int numSquares(int n) {
-        // Initialize a dp array where dp[i] represents the least number of perfect squares summing up to i
         int[] dp = new int[n + 1];
         
-        // Base case: 0 perfect squares needed to sum up to 0
-        dp[0] = 0;
-        
-        // Initialize the dp array with a large value (since we want to find the minimum)
+        // Initialize with a large value
         for (int i = 1; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
-        }
-        
-        // Iterate through each number from 1 to n to fill the dp array
-        for (int i = 1; i <= n; i++) {
-            // Check for all perfect squares less than or equal to i
+            dp[i] = i;  // Maximum is 1^2 + 1^2 + ...
             for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                int square = j * j;
+                dp[i] = Math.min(dp[i], dp[i - square] + 1);
             }
         }
         
-        // The result for n will be stored in dp[n]
         return dp[n];
     }
 }
