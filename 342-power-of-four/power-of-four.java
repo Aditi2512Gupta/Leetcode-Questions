18009d2af118925e8e7d2c5409ba3e18ab523ac1
Aditi Gupta <aditi251gupta@gmail.com>
2025-05-02 22:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (35.23%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -3,11 +3,9 @@ class Solution {
         if (n <= 0) {
             return false;
         }
-        // Keep dividing n by 4 if it's divisible
         while (n % 4 == 0) {
             n /= 4;
         }
-        // If n becomes 1, it is a power of four
         return n == 1;
     }
 }
