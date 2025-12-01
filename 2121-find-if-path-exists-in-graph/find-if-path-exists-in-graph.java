3b13e3aca703cfa74bf0b774df151fb4919ce897
Aditi Gupta <aditi251gupta@gmail.com>
2025-12-01 16:33:41 +0530

    Time: 12 ms (98.53%) | Memory: 240.9 MB (48.62%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,45 +1,23 @@
-import java.util.*;
-
 class Solution {
     int[] parent;
-    int[] rank;
 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         parent = new int[n];
-        rank = new int[n];
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             parent[i] = i;
-            rank[i] = 1;
-        }
 
-        for (int[] e : edges) {
+        for (int[] e : edges)
             union(e[0], e[1]);
-        }
 
         return find(source) == find(destination);
     }
 
     int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
-        }
+        if (parent[x] != x) parent[x] = find(parent[x]);
         return parent[x];
     }
 
     void union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-
-        if (rootA != rootB) {
-            if (rank[rootA] > rank[rootB]) {
-                parent[rootB] = rootA;
-            } else if (rank[rootA] < rank[rootB]) {
-                parent[rootA] = rootB;
-            } else {
-                parent[rootB] = rootA;
-                rank[rootA]++;
-            }
-        }
+        parent[find(a)] = find(b);
     }
 }
