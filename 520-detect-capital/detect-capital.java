acff9478ada36d55caa3c889de7807df89f9ba44
Aditi Gupta <aditi251gupta@gmail.com>
2026-05-10 22:12:44 +0530

    Time: 1 ms (89.70%) | Memory: 43.3 MB (29.03%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -1,11 +1,26 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if (word.equals(word.toUpperCase()))
-            return true;
-        if (word.equals(word.toLowerCase())) 
-            return true;
-        
-        return Character.isUpperCase(word.charAt(0)) &&
-               word.substring(1).equals(word.substring(1).toLowerCase());
+
+        int count = 0;
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                count++;
+            }
+        }
+
+        return count == word.length() || 
+               count == 0 || 
+               (count == 1 && Character.isUpperCase(word.charAt(0)));
     }
 }
+
+
+
+        // if (word.equals(word.toUpperCase()))
+        //     return true;
+        // if (word.equals(word.toLowerCase())) 
+        //     return true;
+        
+        // return Character.isUpperCase(word.charAt(0)) &&
+        //        word.substring(1).equals(word.substring(1).toLowerCase());
