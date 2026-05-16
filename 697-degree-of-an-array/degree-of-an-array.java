5c4e1584a312fd8783f79f391f544e2789f1250d
Aditi Gupta <aditi251gupta@gmail.com>
2026-05-16 22:41:08 +0530

    Time: 27 ms (47.85%) | Memory: 50.9 MB (14.74%) - LeetSync

---
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+    
+        HashMap<Integer, Integer> count = new HashMap<>();
+        HashMap<Integer, Integer> first = new HashMap<>();
+
+        int degree = 0, ans = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            first.putIfAbsent(nums[i], i);
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+            int freq = count.get(nums[i]);
+
+            if (freq > degree) {
+                degree = freq;
+                ans = i - first.get(nums[i]) + 1;
+
+            } 
+            else if (freq == degree) {
+                ans = Math.min(ans,
+                        i - first.get(nums[i]) + 1);
+            }
+        }
+
+        return ans;
+    }
+}
