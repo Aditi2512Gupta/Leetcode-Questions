30a8c61cd7e93eb5de69c95b85cdb362d53d5752
Aditi Gupta <aditi251gupta@gmail.com>
2026-05-26 22:51:29 +0530

    Time: 2 ms (46.14%) | Memory: 46.6 MB (85.83%) - LeetSync

--- 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
@@ -5,7 +5,8 @@ class Solution {
         for(int i = 1; i < nums.length; i++) {
             if(nums[i] > nums[i - 1]) {
                 count++;
-            } else {
+            }
+            else {
                 count = 1;
             }
 
