9c919fddfb3a7431ba55edfac405882a9c974a6e
Aditi Gupta <aditi251gupta@gmail.com>
2026-06-27 22:54:57 +0530

    Time: 4 ms (52.72%) | Memory: 44.3 MB (89.11%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,29 +1,28 @@
 class Solution {
     public List<String> commonChars(String[] words) {
         int[] min = new int[26];
-        Arrays.fill(min, Integer.MAX_VALUE);
+        for (int i = 0; i < 26; i++) 
+            min[i] = 100;
 
-        for (String word : words) {
+        for (String s : words) {
             int[] count = new int[26];
-
-            for (char c : word.toCharArray()) {
-                count[c - 'a']++;
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
             }
-
             for (int i = 0; i < 26; i++) {
-                min[i] = Math.min(min[i], count[i]);
+                if (count[i] < min[i]) {
+                    min[i] = count[i];
+                }
             }
         }
 
         List<String> ans = new ArrayList<>();
 
         for (int i = 0; i < 26; i++) {
-            while (min[i] > 0) {
-                ans.add("" + (char)(i + 'a'));
-                min[i]--;
+            while (min[i]-- > 0) {
+                ans.add(Character.toString((char) ('a' + i)));
             }
         }
-
         return ans;
     }
 }
