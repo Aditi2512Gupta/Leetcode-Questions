441c895ca4d29e26849488a8636b25659a9f152a
Aditi Gupta <aditi251gupta@gmail.com>
2026-07-12 23:16:10 +0530

    Time: 5 ms (38.52%) | Memory: 47.5 MB (23.08%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        HashSet<Integer> set = new HashSet<>();
+       HashSet<Integer> set = new HashSet<>();
+       int n = grid.length;
+       int actualSum = 0, a = 0;
 
-        int n = grid.length;
-        int repeated = 0;
-        int actualSum = 0;
+       for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            actualSum += grid[i][j];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                actualSum += grid[i][j];
-
-                if (set.contains(grid[i][j])) {
-                    repeated = grid[i][j];
-                }
-
-                set.add(grid[i][j]);
+            if(set.contains(grid[i][j])) {
+                a = grid[i][j];
             }
+            set.add(grid[i][j]);
         }
+       }
 
-        int expectedSum = (n * n) * (n * n + 1) / 2;
-        int missing = expectedSum + repeated - actualSum;
+       int expectedSum = ((n * n) * (n * n + 1)) / 2;
+       int b = expectedSum + a - actualSum;
 
-        return new int[]{repeated, missing};
+       return new int[]{a, b};
     }
 }
