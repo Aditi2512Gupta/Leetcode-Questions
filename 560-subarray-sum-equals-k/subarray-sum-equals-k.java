b4a8e1dd33b5d84a950c82a74d9ecd0adfb09afe
Aditi Gupta <aditi251gupta@gmail.com>
2026-07-16 18:44:52 +0530

    Time: 20 ms (98.17%) | Memory: 49.8 MB (5.62%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        // PrefixSum, TC : O(n)
+
+        int n = nums.length;
+        int count = 0;
+
+        int[] prefixSum = new int[n];
+
+        // Build prefix sum array
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int j = 0; j < n; j++) {
+
+            if (prefixSum[j] == k)
+                count++;
+
+            int val = prefixSum[j] - k;
+
+            if (map.containsKey(val))
+                count += map.get(val);
+            
+            map.put(prefixSum[j], map.getOrDefault(prefixSum[j], 0) + 1);
+        }
+
+        return count;
+    }
+}
