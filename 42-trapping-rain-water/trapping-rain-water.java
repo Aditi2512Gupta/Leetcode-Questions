b3900bc791143ae153e906ae8379fdf1effd8012
Aditi Gupta <aditi251gupta@gmail.com>
2026-07-17 23:36:13 +0530

    Time: 1 ms (60.49%) | Memory: 47.7 MB (69.37%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        // 2 Pointer approach TC: O(n)
+
+        int n = height.length;
+        int l = 0, r = n - 1;
+        int lmax = 0, rmax = 0;
+        int ans = 0;
+
+        while(l < r) {
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+
+            if(lmax < rmax) {
+                ans += lmax - height[l];
+                l++;
+            }
+
+            else {
+                ans += rmax - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+}
