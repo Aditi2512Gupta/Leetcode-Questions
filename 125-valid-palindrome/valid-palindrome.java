48028706680e93c3b7b73b7bf8556b847a859c0b
Aditi Gupta <aditi251gupta@gmail.com>
2026-07-20 01:17:43 +0530

    Time: 2 ms (99.30%) | Memory: 44.4 MB (65.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,45 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        // 2 Pointer approach: TC: O(n), SC: O(1)
 
-        int left = 0, right = s.length() - 1;
+        int st = 0, end = s.length() - 1;
 
-        while (left < right) {
+        while(st < end) {
             // Move left pointer forward if it's not alphanumeric
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
+            if(!Character.isLetterOrDigit(s.charAt(st))) {
+                st++;
+                continue;
             }
+
             // Move right pointer backward if it's not alphanumeric
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
+            if(!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+                continue;
             }
 
             // Compare characters (convert to lowercase)
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            if(Character.toLowerCase(s.charAt(st)) != Character.toLowerCase(s.charAt(end))) {
                 return false;
             }
-
-            left++;
-            right--;
+            st++;
+            end--;
         }
 
         return true;
-
-
-       /* StringBuilder filtered = new StringBuilder();
-        
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                filtered.append(Character.toLowerCase(c));
-            }
-        }
-
-        /*String filtered = "";
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                filtered += Character.toLowerCase(c); // Creates a new String object each time
-            }
-        }
-
-        return filtered.toString().equals(filtered.reverse().toString());*/
     }
 }
