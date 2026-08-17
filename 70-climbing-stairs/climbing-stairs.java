586677e54c5eab8fdcbf37b5c17d8c8185c76431
Aditi Gupta <aditi251gupta@gmail.com>
2026-08-17 15:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (17.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,41 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n <= 2)
-            return n;
+        // Recursion + DP (Memoization)
+        
+        int[] dp = new int[n + 1];
 
-        int a = 1, b = 2;
-        for(int i = 3; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
+        return solve(n, dp);
+    }
+
+    private int solve(int n, int[] dp) {
+        if (n <= 1) {
+            return 1;
         }
 
-        return b;
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+        return dp[n];
     }
 }
+
+        //  if (n <= 1) {
+        //     return 1;
+        // }
+
+        // return climbStairs(n - 1) + climbStairs(n - 2);
+
+        // if(n <= 2)
+        //     return n;
+
+        // int a = 1, b = 2;
+        // for(int i = 3; i <= n; i++) {
+        //     int c = a + b;
+        //     a = b;
+        //     b = c;
+        // }
+
+        // return b;
